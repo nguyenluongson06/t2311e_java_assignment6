02e97c9f747fbebdb98c5f33c046c4426704eff9
Nguyễn Duy Hưng <nguyenluongson06@gmail.com>
2024-08-23 22:13:11 +0700

    init commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+﻿import com.model.*;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Person[] people = new Person[3];
+    public static void main(String[] args) {
+
+    }
+
+    public static void enterInfo(){
+        ManagementPerson manager = new ManagementPerson();
+        Scanner input = new Scanner(System.in);
+        System.out.println("=====Management Person program=====");
+        System.out.print("Please input name: ");
+        String name = input.nextLine();
+        System.out.print("Please input address: ");
+        String address = input.nextLine();
+        System.out.print("Please input salary: ");
+        String salary = input.nextLine();
+        try {
+            Person newPerson = manager.inputPersonInfo(name, address, salary);
+            while (newPerson == null){
+                System.out.print("Please input salary:");
+                salary = input.nextLine();
+                newPerson = manager.inputPersonInfo(name, address, salary);
+            }
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+}
---
+++ src/com/model/ManagementPerson.java
@@ -0,0 +1,67 @@
+﻿package com.model;
+
+import java.util.Comparator;
+import java.util.Arrays;
+
+public class ManagementPerson {
+    /**
+     * This is method used to input information of Person
+     *
+     * @param name
+     *            of Person
+     * @param address
+     *            of Person
+     * @param sSalary
+     *            of Person
+     * @return person
+     * @throws Exception "Salary is greater than zero"
+     *                   "You must input Salary."
+     *                   "You must input digit.
+     */
+    public Person inputPersonInfo(String name, String address, String sSalary) throws Exception {
+        double salary = 0;
+        try {
+            if (sSalary == null) throw new Exception("You must input salary");
+            salary = Double.parseDouble(sSalary);
+            if (salary <= 0){
+                throw new Exception("Salary must be greater than zero");
+            }
+        } catch (NumberFormatException e) {
+            throw new Exception("You must input digit");
+        };
+        return new Person(name, address, salary);
+    }
+
+    /**
+     * This is method used to display information of Person you have entered.
+     *
+     * @param person
+     *            Object
+     */
+    public void displayPersonInfo(Person person) {
+        System.out.println("Information of Person you have entered:\n" +
+                "Name:" + person.getName() +"\n" +
+                "Address:" + person.getAddress() +"\n" +
+                "Salary:"+ person.getSalary() + "\n");
+    }
+
+    /**
+     * This is method used to sort Person entered by gradually increased of
+     * Salary.
+     *
+     * @param people
+     *            of Array Person
+     * @return Person Object
+     * @throws Exception "Can't Sort Person"
+     */
+    public Person[] sortBySalary(Person[] people) throws Exception {
+        Person[] result = Arrays.copyOf(people, people.length, Person[].class);
+        try {
+            result = Arrays.stream(people).sorted(Comparator.comparingDouble(Person::getSalary)).toArray(Person[]::new);
+        } catch (Exception e) {
+            throw new Exception("Can't sort people by salary");
+        }
+        return result;
+    }
+}
+
